9bfc7ea3e4afb5b139d6f9552a605808f7901276
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-20 16:46:00 +0900

    20220620

---
+++ June/20220620/test.js
@@ -0,0 +1,21 @@
+function test (){
+  return( `
+  <div></div>
+  <section></section>
+  <div></div>`
+
+  );
+
+}
+
+export default test();
+
+
+// 컴포넌트 개념으로 - > SPA(SINGLE PAGE APPLICATION)
+
+//함수에 그저 문자열로 리턴 하는 개념이 코어 
+//``; 문자열 템플릿으로 필요한 만큼 제어를 하는 방식 
+
+
+//디버깅 압도적으로 유지보수가 편해진다. 
+
